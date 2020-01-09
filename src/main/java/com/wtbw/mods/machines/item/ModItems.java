1f8dba5709fc834057fe06ae6cf94ab6060d1f6c
Naxanria <gerbrandnijk@gmail.com>
2020-01-09 20:52:08 +0100

    Fixed ModBlocks not using Block Added all items to ModItems

---
+++ src/main/java/com/wtbw/mods/machines/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.wtbw.mods.machines.item;
+
+import com.wtbw.mods.machines.WTBWMachines;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraftforge.registries.ObjectHolder;
+
+/*
+  @author: Naxanria
+*/
+@ObjectHolder(WTBWMachines.MODID)
+public class ModItems
+{
+  public static final Item IRON_PLATE = Items.AIR;
+  public static final Item GOLD_PLATE = Items.AIR;
+  
+  public static final Item IRON_DUST = Items.AIR;
+  public static final Item GOLD_DUST = Items.AIR;
+  public static final Item DIAMOND_DUST = Items.AIR;
+  public static final Item EMERALD_DUST = Items.AIR;
+  public static final Item OBSIDIAN_DUST = Items.AIR;
+  public static final Item CHARCOAL_DUST = Items.AIR;
+  public static final Item COAL_DUST = Items.AIR;
+  public static final Item QUARTZ_DUST = Items.AIR;
+  public static final Item ENDER_PEARL_DUST = Items.AIR;
+  
+  public static final Item LAPIS_WAFER = Items.AIR;
+  public static final Item GLOWSTONE_WAFER = Items.AIR;
+}
