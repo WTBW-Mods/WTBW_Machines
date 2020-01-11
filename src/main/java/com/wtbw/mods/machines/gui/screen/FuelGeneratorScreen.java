68c87084362bc6a864c2dcde520861620d840a8b
Sunekaer <kaspar@sunekaer.com>
2020-01-11 15:52:34 +0100

    New fuel gen texture, plus added tip for burn time left.

--- src/main/java/com/wtbw/mods/machines/gui/screen/FuelGeneratorScreen.java
+++ src/main/java/com/wtbw/mods/machines/gui/screen/FuelGeneratorScreen.java
@@ -2,13 +2,20 @@ package com.wtbw.mods.machines.gui.screen;
 
 import com.wtbw.mods.lib.gui.screen.BaseContainerScreen;
 import com.wtbw.mods.lib.gui.util.EnergyBar;
+import com.wtbw.mods.lib.gui.util.ITooltipProvider;
 import com.wtbw.mods.lib.gui.util.SpriteProgressBar;
+import com.wtbw.mods.lib.gui.util.TooltipRegion;
 import com.wtbw.mods.lib.gui.util.sprite.Sprite;
+import com.wtbw.mods.lib.util.TextComponentBuilder;
+import com.wtbw.mods.lib.util.Utilities;
 import com.wtbw.mods.machines.ClientConstants;
 import com.wtbw.mods.machines.gui.container.FuelGeneratorContainer;
 import com.wtbw.mods.machines.tile.generator.FuelGeneratorEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+
+import java.util.List;
 
 /*
   @author: Naxanria
@@ -49,6 +56,12 @@ public class FuelGeneratorScreen extends BaseContainerScreen<FuelGeneratorContai
     energyBar = getDefaultBar(tileEntity.getStorage());
     progressBar = new SpriteProgressBar(81, guiTop + 30, progressSprite, progressBackgroundSprite, tileEntity::getGenTime, tileEntity::getGenCounter);
     addTooltipProvider(energyBar);
+    addTooltipProvider(new TooltipRegion(guiLeft + 81, guiTop + 30, 14,14) {
+      @Override
+      public List<String> getTooltip() {
+        return Utilities.listOf( TextComponentBuilder.createTranslated("wtbw_machines.tooltip.burn_time", String.valueOf(tileEntity.getGenCounter())).build().getFormattedText());
+      }
+    });
   }
   
   @Override
