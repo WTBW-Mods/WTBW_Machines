ea193213b4a112a863cb9cdecd928073f1eaa1c6
Naxanria <gerbrandnijk@gmail.com>
2020-05-01 20:29:38 +0200

    Wrench now uses tag forge:tools/wrench

--- src/main/java/com/wtbw/mods/machines/block/util/WrenchHelper.java
+++ src/main/java/com/wtbw/mods/machines/block/util/WrenchHelper.java
@@ -1,19 +1,15 @@
 package com.wtbw.mods.machines.block.util;
 
-import com.wtbw.mods.lib.gui.util.sprite.Direction;
 import com.wtbw.mods.lib.util.Utilities;
 import com.wtbw.mods.machines.Constants;
 import com.wtbw.mods.machines.item.WrenchItem;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.DirectionProperty;
-import net.minecraft.util.ActionResultType;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
 
 /*
   @author: Naxanria
@@ -23,7 +19,7 @@ public class WrenchHelper
   public static boolean isUsingWrench(PlayerEntity playerEntity, Hand hand)
   {
     ItemStack stack = playerEntity.getHeldItem(hand);
-    return  (stack.getItem() instanceof WrenchItem);
+    return stack.getItem().isIn(ItemTags.getCollection().get(new ResourceLocation("forge:tools/wrench")));
   }
   
   public static WrenchItem.WrenchAction horizontalRotationWrenchAction(DirectionProperty facing)
